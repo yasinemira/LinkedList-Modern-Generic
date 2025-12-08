e96377d62552a86518af91870e291e1817064c38
yasinemira <yasinemira@gmail.com>
2025-12-08 07:51:59 +0100

    Split LinkedList implementation as a stand-alone header

---
+++ LinkedList.h
@@ -0,0 +1,280 @@
+#include <iostream>
+#include <memory>
+
+/**
+ * Linked-List implementation using modern C++*
+ */
+
+template <typename T>
+using sp = std::shared_ptr<T>;
+
+template <typename T>
+struct Node
+{
+    Node(T val)
+        : value{val},
+          next{nullptr}
+    {
+    }
+
+    T value;
+    sp<Node> next;
+};
+
+template <typename T>
+class LinkedList
+{
+public:
+    
+    LinkedList() = default;
+
+    LinkedList(T val)
+    {
+        auto node = std::make_shared<Node<T>>(val);
+        m_head = node;
+        m_tail = node;
+        ++m_length;
+    }
+
+    // Time-complexity: O(1)
+    void append(T val)
+    {
+        auto newNode = std::make_shared<Node<T>>(val);
+        m_tail->next = newNode;
+        m_tail = newNode;
+        ++m_length;
+    }
+
+    // Time-complexity: O(n)
+    void pop()
+    {
+        if (!m_head)
+        {
+            throw std::out_of_range("no node available/linked-list is empty!");
+        }
+
+        if (m_length == 1)
+        {
+            m_head.reset();
+            m_tail.reset();
+            --m_length;
+        }
+
+        auto temp = m_head;
+        while (temp->next != m_tail)
+        {
+            temp = temp->next;
+        }
+        m_tail = temp;
+        m_tail->next = nullptr;
+        --m_length;
+    }
+
+    // Time-complexity: O(1)
+    void prepend(T val)
+    {
+        auto newNode = std::make_shared<Node<T>>(val);
+        if (m_length == 0)
+        {
+            m_head = newNode;
+            m_tail = newNode;
+        }
+        else
+        {
+            newNode->next = m_head;
+            m_head = newNode;
+        }
+        ++m_length;
+    }
+
+    // Time-complexity: O(1)
+    void popFirst()
+    {
+        if (!m_head)
+        {
+            throw std::out_of_range("no node available/the linked-list is empty!");
+        }
+        if (m_length == 1)
+        {
+            m_head.reset();
+            m_tail.reset();
+        }
+        else
+        {
+            m_head = m_head->next;
+        }
+        --m_length;
+    }
+
+    // Time-complexity: O(n)
+    T get(int index)
+    {
+        if (index < 0 || index >= m_length)
+        {
+            throw std::out_of_range("Invalid index!");
+        }
+
+        sp<Node<T>> temp = m_head;
+        for (size_t i = 0; i < index; ++i) {
+            temp = temp->next;
+        }
+
+        std::cout << "Value at the index #" << index << " is: " << temp->value << std::endl;
+        return temp->value;
+    }
+
+    // Time-complexity: O(n)
+    void set(int index, T val)
+    {
+        if (index < 0 || index >= m_length) {
+            throw std::out_of_range("Invalid index!");
+        }
+
+        auto temp = m_head;
+        for (size_t i = 0; i < index; ++i) {
+            temp = temp->next;
+        }
+        temp->value = val;
+        std::cout << "Value at the index #" << index << " is now: " << val << std::endl;
+    }
+
+    // Time-complexity: O(n)
+    void insert(int index, T val) {
+        if (index < 0 || index > m_length) {
+            throw std::out_of_range("Invalid index!");
+        }
+
+        auto newNode = std::make_shared<Node<T>>(val);
+        auto temp = m_head;
+
+        if (index == 0) {
+            prepend(val);
+            return;
+        }
+
+        if (index == m_length) {
+            append(val);
+            return;
+        }
+
+        // go to the index to insert
+        for (size_t i = 0; i < index - 1; ++i) {
+            temp = temp->next;
+        }
+        newNode->next = temp->next;
+        temp->next = newNode;
+        ++m_length;
+    }
+
+    // Time-complexity: O(n)
+    void remove(int index) {
+        if (index < 0 || index >= m_length) {
+            throw std::out_of_range("Invalid index!");
+        }
+        
+        if (index == 0) {
+            popFirst();
+            return;
+        }
+
+        if (index == m_length - 1) {
+            pop();
+            return;
+        }
+
+        auto temp = m_head;
+        for (size_t i = 0; i < index - 1; ++i) {
+            temp = temp->next;
+        }
+        temp->next = temp->next->next;
+        --m_length;
+    }
+
+    void reverse()
+    {
+        if (m_length <= 0) {
+            throw std::runtime_error("Invalid operation!");
+        }
+        
+        if(m_length == 1) {
+            return;
+        }
+
+        //first, swap head and tail
+        auto temp = m_head;
+        m_head = m_tail;
+        m_tail = temp;
+
+        //second, introduce "before" and "after" trackers
+        sp<Node<T>> before = nullptr;
+        sp<Node<T>> after = nullptr;
+
+        // before - temp - after pointers will move in harmony
+        while (temp != nullptr) {
+            after = temp->next;
+            temp->next = before;
+            before = temp;
+            temp = after;
+        }
+    }
+
+    T getHead() const
+    {
+        if (!m_head)
+        {
+            throw std::out_of_range("no node available/linked-list is empty!");
+        }
+        std::cout << "head:" << m_head->value << std::endl;
+        return m_head->value;
+    }
+
+    T getTail() const
+    {
+        if (!m_tail)
+        {
+            throw std::out_of_range("no node available/linked-list is empty!");
+        }
+        std::cout << "tail: " << m_tail->value << std::endl;
+        return m_tail->value;
+    }
+
+    int getLength() const
+    {
+        std::cout << "length: " << m_length << std::endl;
+        return m_length;
+    }
+
+    void printLinkedList() const
+    {
+        auto temp = m_head;
+        while (temp)
+        {
+            std::cout << temp->value << " ";
+            temp = temp->next;
+        }
+        std::cout << "\n";
+    }
+
+    static const LinkedList<T> createLinkedList()
+    {
+        //initializing with some dummy values and also indirectly testing the linkedlist-related functions
+        auto ll = LinkedList(11);
+        ll.append(16);
+        ll.append(5);
+        ll.prepend(77);
+        ll.pop();
+        ll.append(-2);
+        ll.prepend(11);
+        ll.getHead();
+        ll.getTail();
+        ll.set(2, 24);
+        ll.printLinkedList();
+        return ll;
+    }
+
+private:
+    sp<Node<T>> m_head;
+    sp<Node<T>> m_tail;
+    int m_length{0};
+};
+ 
